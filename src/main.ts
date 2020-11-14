2bf9b7c26dbfaa36c51797c80b27b7ce4a875024
Tyubek Vasiliy <v.tyubek@gmail.com>
2020-11-15 00:34:56 +0300

    initial setup

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import path from 'path';
+import * as tc from '@actions/tool-cache';
+import * as core from '@actions/core';
+
+export async function run() {
+  try {
+    const downloadPath = await tc.downloadTool("https://github.com/vasiliy-t/tarantool-versions/releases/download/2.7.0/tarantool.tar.gz", undefined, undefined);
+    core.info('Extracting Tarantool...');
+    let extPath = await tc.extractZip(downloadPath);
+    core.info(`Successfully extracted go to ${extPath}`);
+
+    core.addPath(path.join(extPath, 'bin'));
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
